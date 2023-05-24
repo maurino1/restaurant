bb5c884354112a8966e2132d9694b07e1953e1a3
mau <9014816@student.zadkine.nl>
2023-05-24 09:48:34 +0200

    test 3.o

--- aanmeldForm2.php
+++ aanmeldForm2.php
@@ -2,8 +2,12 @@
     require "werknemer.php";
 
     echo "<h1>Aanmeldformulier werknemer </h1>";
+    $werknemerNaam=$_POST["naam"];
     $werknemerEmail=$_POST["emailvak"];
     $werknemerWachtwoord=$_POST["wachtwoordvak"];
+    $werknemerTelefoonNummer=$_POST["telefoonnummer"];
+    $werknemerAdress=$_POST["adress"];
+    $werknemerPostcode=$_POST["postcode"];
     $werknemer1 = new werknemer($werknemerEmail,$werknemerWachtwoord);
     $werknemer1->aanmelden();
 
@@ -11,4 +15,4 @@
     $werknemer1->afdrukkenWerknemer();
 
     echo "<br><br/><a href='hoofdmenu.php'>terug naar hoofdmenu</a>";
-?>
+
